5e87cd219aeb6761258e51e7f8e0f144cdfc8497
Ali Rahbar <alirahbar2005@gmail.com>
2025-07-01 14:45:43 -0400

    fixde bugs

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -140,7 +140,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
